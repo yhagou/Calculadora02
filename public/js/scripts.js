de44e3794577d6c9c04388f57618fa97f64e9006
yhagou <yhago.luis@discent.ufma.br>
2023-11-20 21:06:14 -0300

    new update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -12,9 +12,14 @@ function back(){
    document.getElementById("resultado") .innerHTML = resultado.substring(0, resultado.length - 1);
 }
 function calcular(){
- var resultado = document.getElementById('resultado').innerHTML;
+  var resultado = document.getElementById('resultado').innerHTML;
+  if(resultado){
  document.getElementById('resultado').innerHTML = eval(resultado);
-  
+}
+else {
+    document.getElementById('resultado').innerHTML = 'Nada para calcular'
+
+}
 }
 
 module.exports = {insert, clean, back, calcular}
